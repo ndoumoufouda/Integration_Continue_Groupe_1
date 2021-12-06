7d0aad3f57fff36c1fbb31b48da994caea35578b
PatSin55 <kamdempatrick550@gmail.com>
2021-12-06 22:09:09 +0100

    modifpatrick

--- src/main/java/org/isj/calculatriceserver/CalculatriceserverApplication.java
+++ src/main/java/org/isj/calculatriceserver/CalculatriceserverApplication.java
@@ -1,3 +1,4 @@
+
 package org.isj.calculatriceserver;
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/org/isj/calculatriceserver/service/Operations.java
+++ src/main/java/org/isj/calculatriceserver/service/Operations.java
@@ -1,3 +1,4 @@
+//Kamdem Patrick gere la partie ci
 package org.isj.calculatriceserver.service;
 
 public interface Operations {
