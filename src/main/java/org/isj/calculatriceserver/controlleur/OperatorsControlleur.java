0ec9182129db1aada68ba65546efd08feafe54dd
FoudaAndre <ndoumouandre@gmail.com>
2021-12-06 17:07:46 +0100

    nouvelle structure de base du projet

--- src/main/java/org/isj/calculatriceserver/controlleur/OperatorsControlleur.java
+++ src/main/java/org/isj/calculatriceserver/controlleur/OperatorsControlleur.java
@@ -1,4 +1,7 @@
 package org.isj.calculatriceserver.controlleur;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OperatorsControlleur {
 }
--- src/main/java/org/isj/calculatriceserver/serviceImp/OperationsImp.java
+++ src/main/java/org/isj/calculatriceserver/serviceImp/OperationsImp.java
@@ -1,6 +1,8 @@
 package org.isj.calculatriceserver.serviceImp;
 
 import org.isj.calculatriceserver.service.Operations;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OperationsImp implements Operations {
 }
