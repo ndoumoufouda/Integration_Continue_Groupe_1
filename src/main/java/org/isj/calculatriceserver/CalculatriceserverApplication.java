a7bd733e36a1b1a4a7a58e4ac0c978e54cd09a47
FoudaAndre <ndoumouandre@gmail.com>
2021-12-21 10:46:08 +0100

    modification du projet pour heroku

--- src/main/java/org/isj/calculatriceserver/CalculatriceserverApplication.java
+++ src/main/java/org/isj/calculatriceserver/CalculatriceserverApplication.java
@@ -2,10 +2,23 @@ package org.isj.calculatriceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class CalculatriceserverApplication {
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "choisir l'operation que vous souhaiter effectuer sur notre calculatrice \n" +
+                "1 - Addition \n" +
+                "2 - soustraction\n" +
+                "3 - division \n" +
+                "4 - multiplication \n" +
+                "5 - Division \n" +
+                "autre valeur va fermer l'application";
+    }
     public static void main(String[] args) {
         SpringApplication.run(CalculatriceserverApplication.class, args);
     }
