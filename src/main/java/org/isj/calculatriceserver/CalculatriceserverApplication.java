52b948b3041aeeaf1e43fefa23a6882334245405
FoudaAndre <ndoumouandre@gmail.com>
2021-12-21 16:39:12 +0100

    modification du projet pour heroku

--- src/main/java/org/isj/calculatriceserver/CalculatriceserverApplication.java
+++ src/main/java/org/isj/calculatriceserver/CalculatriceserverApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @Controller
+@SpringBootApplication
 public class CalculatriceserverApplication {
 
     @RequestMapping("/")
